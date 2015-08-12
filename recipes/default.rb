92354a9abbce851a4ee18974e56cd56f074677e8
Philippe EYCHART <philippe.eychart@informatique.gov.pf>
2015-08-12 11:22:08 -1000

    correction bug de changement de type de l'attribut

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,7 +22,7 @@
 $getEnv= lambda { |context, val|
   val.each do |n, v|
     if v.is_a? Hash
-      if ( n[0]=='!' || context[ n[0]=='!' ? n[1..-1] : n ]=={} )
+      if ( n[0]=='!' || context[ n[0]=='!' ? n[1..-1] : n ]=={} || context[ n[0]=='!' ? n[1..-1] : n ]==[] || context[ n[0]=='!' ? n[1..-1] : n ]=='')
            context[ n[0]=='!' ? n[1..-1] : n ] = v
       else context[ n[0]=='!' ? n[1..-1] : n ] = $getEnv.call( context[ n[0]=='!' ? n[1..-1] : n ], v )
       end
